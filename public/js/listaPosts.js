74cea6b4379988581952bb43fe32d7698b7a7159
Caduviana <carloseduardov.uba@gmail.com>
2025-10-22 19:25:35 -0300

    Configuração do javascript

---
+++ public/js/listaPosts.js
@@ -0,0 +1,19 @@
+const filterButton = document.getElementById('filter-button');
+const filterOptions = document.getElementById('filter-options');
+const closeButton = document.getElementById('close-button');
+const searchButton = document.getElementById('search-button');
+
+
+filterButton.addEventListener('click', () => {
+    filterOptions.classList.toggle('show');
+    closeButton.classList.toggle('show');
+    if(closeButton.style.display === "none" || closeButton.style.display === ""){
+        closeButton.style.display = "flex";
+    } else {
+        closeButton.style.display = "none";
+    }   
+});
+
+filterButton.addEventListener('click', () => {
+    searchButton.classList.toggle('movido');
+});
