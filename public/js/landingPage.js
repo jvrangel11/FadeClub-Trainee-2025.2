0eed233dc12d8f59f09c65590ab693f2cecc04c6
Breno <brenomachado.3064@gmail.com>
2025-10-27 00:27:47 -0300

    Primeiro teste de JS

--- public/js/landingPage.js
+++ public/js/landingPage.js
@@ -19,6 +19,66 @@ function updateCarrossel()
     const itemWidth = sliderContent.children[0].getBoundingClientRect().width;
 
     itemsPerView = Math.max(1, Math.floor(sliderWidth / itemWidth));
-    totalPages = Math.ceil( (sliderContent.children.length / itemsPerView) -1);
+    totalPages = Math.ceil( (sliderContent.children.length / itemsPerView) - 2*((sliderWidth / itemWidth)/100));
 
 }
+
+// ! Movimentação
+
+function scrollToPage ()
+{
+    const newPosition = slider.offsetWidth * currentPage;
+    sliderContent.scrollTo({ left:newPosition, behavior: 'smooth'});
+    resetAutoSlide();
+}
+
+function moveLeft()
+{
+    currentPage--;
+    if(currentPage < 0)
+    {
+        currentPage = totalPages-1;
+    }
+    scrollToPage();
+    resetAutoSlide();
+}
+
+function moveRight()
+{
+    currentPage++;
+    if(currentPage >= totalPages)
+    {
+        currentPage = 0;
+    }
+    scrollToPage();
+    resetAutoSlide();
+}
+
+//! Slide automático
+
+function startAutoSlide()
+{
+    autoSlideInterval = setInterval( () => {
+        moveRight();
+    }, 4000)
+}
+
+function resetAutoSlide()
+{
+    clearInterval(autoSlideInterval);
+    startAutoSlide();
+}
+
+
+//! Eventos 
+
+leftArrow1.addEventListener('click', moveLeft);
+leftArrow2.addEventListener('click', moveLeft);
+rightArrow1.addEventListener('click', moveRight);
+rightArrow2.addEventListener('click', moveRight);
+window.addEventListener('resize', updateCarrossel);
+
+//! Chama inicio das funções 
+
+updateCarrossel();
+startAutoSlide();
