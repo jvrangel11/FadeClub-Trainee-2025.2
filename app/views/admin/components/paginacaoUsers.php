70831e6929e82078e4ba21fcaff913303a63e79a
Breno <brenomachado.3064@gmail.com>
2025-12-01 02:08:38 -0300

    configura botões da paginação

--- app/views/admin/components/paginacaoUsers.php
+++ app/views/admin/components/paginacaoUsers.php
@@ -3,10 +3,13 @@
         <button class="botaoEsquerda">&#9664</button>
     </div>
     <div class="botoesPaginacao">
-        <button class="botaoPaginacao">1</button>
-        <button class="botaoPaginacao">2</button>
-        <button class="botaoPaginacao">3</button>
-        <button class="botaoPaginacao">4</button>
+        <?php for($page_number = 1; $page_number <= 4; $page_number++): ?>
+            <button>
+                <a class="botaoPaginacao <?= $page_number == 3 ? "active" : "" ?> " href="?paginacaoNumero=<?= $page_number?>">
+                    <?= $page_number ?>
+                </a>
+            </button>
+        <?php endfor ?>
     </div>
     <div>
         <button  class="botaoDireita">&#9654</button>
