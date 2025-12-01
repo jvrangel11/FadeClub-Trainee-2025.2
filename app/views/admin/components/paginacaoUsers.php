6dbd58e8bf728279a7794dffdac4ea91bb3be2fa
Breno <brenomachado.3064@gmail.com>
2025-12-01 01:02:37 -0300

    Passa a paginação para um arquivo próprio

---
+++ app/views/admin/components/paginacaoUsers.php
@@ -0,0 +1,14 @@
+<div class="paginacao">
+    <div>
+        <button class="botaoEsquerda">&#9664</button>
+    </div>
+    <div class="botoesPaginacao">
+        <button class="botaoPaginacao">1</button>
+        <button class="botaoPaginacao">2</button>
+        <button class="botaoPaginacao">3</button>
+        <button class="botaoPaginacao">4</button>
+    </div>
+    <div>
+        <button  class="botaoDireita">&#9654</button>
+    </div>
+</div>
