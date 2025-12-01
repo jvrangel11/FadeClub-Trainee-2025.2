81c0440533917b7e22fa6cfc8456532d9f2863ec
Breno <brenomachado.3064@gmail.com>
2025-12-01 03:38:11 -0300

    ajuste adicional ao botão de criar

--- app/views/admin/tabelaUsuarios.view.php
+++ app/views/admin/tabelaUsuarios.view.php
@@ -23,7 +23,13 @@
     <div class="containerGeral">
         <header>
             <h1>Tabela de Usuários</h1>
-            <button class="criar" onclick="abrirModal('criar')">Criar Usuário</button>
+            <button class="criar" onclick="abrirModal('criar')">
+                <svg class="criarButton" width="14" height="14" viewBox="0 0 14 14" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M14 8H8V14H6V8H0V6H6V0H8V6H14V8Z" fill="#ffffff"/>
+                </svg>
+
+                <p>Criar Usuário</p>
+            </button>
         </header>
         <div class="scroll-lateral">
             <table>
