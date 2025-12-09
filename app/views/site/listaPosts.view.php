992eed0aa78735c22e2a76bfa6a603551a3a6c97
carloseduv <carloseduardov.uba@gmail.com>
2025-12-09 14:05:45 -0300

    imagem da logo colocada na guia da página

--- app/views/site/listaPosts.view.php
+++ app/views/site/listaPosts.view.php
@@ -13,6 +13,7 @@
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
 
+  <link rel="icon" type="image/png" href="../../../public/assets/Imagens/logo.png"/>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Projeto FadeClub</title>
