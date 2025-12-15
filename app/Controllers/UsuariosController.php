c7cc6b890aaf4c57081ef9abbda476a13ab2a5ea
Breno <brenomachado.3064@gmail.com>
2025-12-14 23:03:57 -0300

    Ajusta imagem padrão

--- app/Controllers/UsuariosController.php
+++ app/Controllers/UsuariosController.php
@@ -39,7 +39,7 @@ class UsuariosController
 
     private function uploadFile($file){
         if(!isset($file) || $file['error'] !== UPLOAD_ERR_OK){
-            return null;
+            return './public/assets/Imagens/usuario.png';
         }
 
         if(!file_exists(self::UPLOAD_DIR)){
@@ -71,7 +71,7 @@ class UsuariosController
 
     public function store()
     {
-        $imgPath = $this->uploadFile($_FILES['img_path'] ?? null);
+        $imgPath = $this->uploadFile($_FILES['img_path'] ?? './public/assets/Imagens/usuario.png' );
 
 
         $parameters = [
